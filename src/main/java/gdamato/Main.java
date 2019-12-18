5145ac8071f14645cc006698cdccac0dd820acdc
Gianfranco Damato <gianfranco.damato@hotmail.it>
2019-12-18 17:41:52 +0100

    Able to load an .arff file now.

--- src/main/java/gdamato/Main.java
+++ src/main/java/gdamato/Main.java
@@ -4,12 +4,36 @@ import gdamato.core.RoughConceptClassifier;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 
 public class Main {
+    private static RoughConceptClassifier RCC;
+
     public static void main(String[] args) {
         System.out.println("Hello, world!");
-        RoughConceptClassifier RCC = new RoughConceptClassifier();
+        RCC = new RoughConceptClassifier();
+        LoadDataSet();
+
+    }
+
+    public static void BuildNewDataset() {
         try {
             RCC.LoadOntology("C:\\Users\\Gianf\\Dropbox\\Tesi\\Ontologie\\wine.owl");
         } catch (OWLOntologyCreationException e) {
+            e.printStackTrace();
+        }
+
+        RCC.BuildProjectionTable();
+
+        try {
+            RCC.ExportAsWekaARFF("C:\\Users\\Gianf\\Desktop\\table.arff");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void LoadDataSet() {
+        try {
+            RCC.LoadWekaARFF("C:\\Users\\Gianf\\Desktop\\table.arff");
+            RCC.ComputePCA();
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
--- src/main/java/gdamato/core/RoughConceptClassifier.java
+++ src/main/java/gdamato/core/RoughConceptClassifier.java
@@ -1,23 +1,36 @@
 package gdamato.core;
 
-
+import gdamato.types.ProjectionValue;
 import org.semanticweb.HermiT.ReasonerFactory;
 import org.semanticweb.owlapi.apibinding.OWLManager;
-import org.semanticweb.owlapi.model.OWLOntology;
-import org.semanticweb.owlapi.model.OWLOntologyCreationException;
-import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.model.*;
 import org.semanticweb.owlapi.reasoner.OWLReasoner;
+import org.semanticweb.owlapi.util.SimpleShortFormProvider;
+import weka.attributeSelection.PrincipalComponents;
+import weka.core.Attribute;
+import weka.core.DenseInstance;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
+import weka.core.converters.ConverterUtils.DataSink;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class RoughConceptClassifier {
 
 
     //Ontology stuff
 
-    OWLOntologyManager ontMgr;
-    OWLOntology onto;
-    OWLReasoner reasoner;
+    private OWLOntologyManager ontMgr;
+    private OWLOntology onto;
+    private OWLReasoner reasoner;
+    private OWLDataFactory dataFactory;
+
+    //Projection stuff
+    private Instances projectionTable;
 
 
     public RoughConceptClassifier() {
@@ -27,11 +40,85 @@ public class RoughConceptClassifier {
     public void LoadOntology(String path_to) throws OWLOntologyCreationException {
         onto = ontMgr.loadOntologyFromOntologyDocument(new File(path_to));
         reasoner = new ReasonerFactory().createReasoner(onto);
-        System.out.println("Is this ontology consistent?" + reasoner.isConsistent());
+        dataFactory = ontMgr.getOWLDataFactory();
     }
 
 
     public void BuildProjectionTable() {
+        ArrayList<Attribute> attributes = new ArrayList<>();
+        SimpleShortFormProvider sfp = new SimpleShortFormProvider();
+        //Jesus Christ how awful
+        Set<OWLClass> classes = onto.classesInSignature().collect(Collectors.toSet());
+        System.out.println("Classes in this ontology: " + classes.size());
+        for(OWLClass c : classes) {
+            attributes.add(new Attribute(sfp.getShortForm(c)));
+        }
+
+        projectionTable = new Instances("OntologyTest", attributes, 0);
+
+        Set<OWLNamedIndividual> individuals = onto.individualsInSignature().collect(Collectors.toSet());
+        System.out.println("Individuals in this ontology: " + individuals.size());
+
+        for(OWLNamedIndividual i:individuals) {
+            double bufferData[] = new double[classes.size()];
+            int idx = 0;
+            for(OWLClass c: classes) {
+                System.out.print(String.format("Computing %s(%s):",sfp.getShortForm(c),sfp.getShortForm(i)));
+                ProjectionValue computedValue = computeProjectionValue(c, i);
+                System.out.println(computedValue);
+                bufferData[idx] = mapValueToNumber(computedValue);
+                idx++;
+            }
+            projectionTable.add(new DenseInstance(1.0, bufferData));
+        }
+    }
+
+    private ProjectionValue computeProjectionValue(OWLClass c, OWLNamedIndividual i) {
+        ProjectionValue v = ProjectionValue.UNCERTAIN;
+        OWLAxiom instanceCheck = dataFactory.getOWLClassAssertionAxiom(c, i);
+        if (reasoner.isEntailed(instanceCheck)) {
+            v = ProjectionValue.TRUE;
+        } else {
+            OWLAxiom complementInstanceCheck = dataFactory.getOWLClassAssertionAxiom(c.getObjectComplementOf(), i);
+            if (reasoner.isEntailed(complementInstanceCheck)) {
+                v = ProjectionValue.FALSE;
+            }
+        }
+        return v;
+    }
+
+    private double mapValueToNumber(ProjectionValue value) {
+        double mapping = 0;
+        switch(value) {
+            case TRUE:
+                mapping = 1;
+                break;
+            case FALSE:
+                mapping = 0;
+                break;
+            case UNCERTAIN:
+                mapping = 0.5;
+                break;
+        }
+        return mapping;
+    }
+
+    public void ExportAsWekaARFF(String path_to)  throws Exception{
+            DataSink.write(path_to, projectionTable);
+    }
+
+    public void LoadWekaARFF(String path_to) throws Exception {
+        DataSource ds = new DataSource(path_to);
+        projectionTable = ds.getDataSet();
+    }
+
+    public void ComputePCA() throws Exception {
+        PrincipalComponents pca = new PrincipalComponents();
+        pca.buildEvaluator(projectionTable);
+        for(int i = 0; i < projectionTable.numAttributes(); i++) {
+
+            System.out.println(String.format("%d) Attribute: %s. Eigenvalue: %f", (i+1), projectionTable.attribute(i).name(), pca.getEigenValues()[i]));
+        }
 
     }
 
